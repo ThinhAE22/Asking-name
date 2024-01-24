df64b557d94ef8f94d8022d73cae70a254eb53ef
Thinh Tran <153496070+ThinhAE22@users.noreply.github.com>
2024-01-24 15:02:17 +0200

    Add files via upload

---
+++ L1_Asking_name/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class HelloStranger
+{
+    static void Main()
+    {
+        string answer;
+        Console.Write("Indentify yourself: "); 
+        answer = Console.ReadLine(); // ask for input
+        Console.WriteLine("Nice to meet you " + answer); // Make a new line
+    }
+}
